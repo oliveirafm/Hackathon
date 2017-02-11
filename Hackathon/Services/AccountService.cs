ead8986d1c64472e08211e089ac19caa70aca29c
Ivo Valente <ivovalente@hotmail.com>
2017-02-11 22:55:27 +0000

    Added Nethereum libraries ands blochain services

---
+++ Hackathon/Services/AccountService.cs
@@ -0,0 +1,51 @@
+﻿using Nethereum.Web3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Hackathon.Services
+{
+    public class AccountService : BaseService
+    {
+
+        public string passPhrase = "ThE~Bl0ckch@in-P@ssW0rd";
+
+
+        public AccountService()
+        {
+        }
+
+        public AccountService(Web3 web3)
+        {
+            this.web3 = web3;
+        }
+
+        public async Task<string> NewAccount()
+        {
+            var accountAddress = await web3.Personal.NewAccount.SendRequestAsync(this.passPhrase);
+
+            return accountAddress;
+        }
+
+        public async Task<string> NewAccount(string passPhrase)
+        {
+            var accountAddress = await web3.Personal.NewAccount.SendRequestAsync(passPhrase);
+
+            return accountAddress;
+        }
+
+        public async Task<List<string>> GetAccountAddressesList()
+        {
+            var accountAddresses = await web3.Personal.ListAccounts.SendRequestAsync();
+
+            //var accountAddresses = await web3.Eth.Accounts.SendRequestAsync();
+
+            return accountAddresses.ToList();
+        }
+
+
+
+    }
+}
